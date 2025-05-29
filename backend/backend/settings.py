09dd9f37dd2f41f637f94ec7f864f408dd5f2025
iabyyy <abhiramab3631@gmail.com>
2025-05-29 10:32:07 +0530

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -67,11 +68,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default":dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 # Password validation
